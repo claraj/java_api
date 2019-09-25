e9d25ca55239aff04af7bb4254ff255b6d7c7139
clara <clara@minneapolis.edu>
2019-09-25 11:31:03 -0500

    cleanup

--- src/main/java/YelpResponse.java
+++ src/main/java/YelpResponse.java
@@ -5,39 +5,17 @@ import java.util.Arrays;
  */
 public class YelpResponse {
     public Business[] businesses;
-    
-    @Override
-    public String toString() {
-        return "YelpResponse{" +
-                "businesses=" + Arrays.toString(businesses) +
-                '}';
-    }
 }
 
 class Business {
     public double rating;
     public String name;
     public Location location;
-    
-    @Override
-    public String toString() {
-        return "Business{" +
-                "rating=" + rating +
-                ", name='" + name + '\'' +
-                ", location=" + location +
-                '}';
-    }
+
 }
 
 class Location {
     public String city;
     public String address1;
-    
-    @Override
-    public String toString() {
-        return "Location{" +
-                "city='" + city + '\'' +
-                ", address1='" + address1 + '\'' +
-                '}';
-    }
+
 }

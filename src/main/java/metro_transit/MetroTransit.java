71c67ba99982b79a23a422f2a3a726b95202d83b
clara <10088152+claraj@users.noreply.github.com>
2022-11-30 16:40:02 -0600

    Update Metro Transit for their new (Nov 22) API response structure

--- src/main/java/metro_transit/MetroTransit.java
+++ src/main/java/metro_transit/MetroTransit.java
@@ -11,8 +11,52 @@ public class MetroTransit {
     *  Use the Metro Transit API to get the next bus departures for the stop outside
     * Minneapolis College.
     *
-    * The response is a JSON array of objects, so when deserializing, the data,
+    * The response is a JSON object. One of the keys, "departures"
+    * contains an array of objects, so when deserializing, the data,
     * it is converted into an array of BusStatus objects.
+    *
+    * {
+        "stops": [
+            {
+                "stop_id": 17940,
+                "latitude": 44.973,
+                "longitude": -93.284325,
+                "description": "Hennepin Ave & MCTC"
+            }
+        ],
+        "alerts": [],
+        "departures": [
+            {
+                "actual": true,
+                "trip_id": "22197530-AUG22-MVS-BUS-Weekday-01",
+                "stop_id": 17940,
+                "departure_text": "4 Min",
+                "departure_time": 1669847560,
+                "description": "Downtown / Via Lyndale",
+                "route_id": "4",
+                "route_short_name": "4",
+                "direction_id": 0,
+                "direction_text": "NB",
+                "schedule_relationship": "Scheduled"
+            },
+            {
+                "actual": true,
+                "trip_id": "22197531-AUG22-MVS-BUS-Weekday-01",
+                "stop_id": 17940,
+                "departure_text": "4 Min",
+                "departure_time": 1669847571,
+                "description": "Johnson St / Silver Lk Vlg / Via Penn",
+                "route_id": "4",
+                "route_short_name": "4",
+                "direction_id": 0,
+                "direction_text": "NB",
+                "terminal": "B",
+                "schedule_relationship": "Scheduled"
+            },
+
+            ... more buses ...
+            ]
+        }
     *
     * */
     
@@ -22,7 +66,9 @@ public class MetroTransit {
         // is the stop on Hennepin Avenue for buses traveling north.
         String metroTransitURL = "http://svc.metrotransit.org/NexTrip/17940?format=json";
 
-        BusStatus[] busStatuses = Unirest.get(metroTransitURL).asObject(BusStatus[].class).getBody();
+        MetroTransitResponse response = Unirest.get(metroTransitURL).asObject(MetroTransitResponse.class).getBody();
+
+        BusStatus[] busStatuses = response.departures;
 
         String busTableTemplate = "%-10s%-40s%-20s\n";
 
@@ -31,29 +77,18 @@ public class MetroTransit {
         System.out.println("=".repeat(70));
 
         // Read information about each bus, display in table form
+
         for (BusStatus bus: busStatuses) {
-            System.out.printf(busTableTemplate, bus.Route, bus.Description, bus.DepartureText);
+            System.out.printf(busTableTemplate, bus.route_id, bus.description, bus.departure_text);
         }
     }
 }
 
+class MetroTransitResponse {
+    BusStatus[] departures;
+}
 class BusStatus {
-    public String VehicleLongitude;
-    public String DepartureText;
-    public String DepartureTime;
-    public String RouteDirection;
-    public String Description;
-    public String VehicleLatitude;
-    public String Actual;
-    public String Gate;
-    public String BlockNumber;
-    public String Terminal;
-    public String Route;
-    public String VehicleHeading;
-    
-    @Override
-    public String toString()
-    {
-        return "BusStatus [VehicleLongitude = "+VehicleLongitude+", DepartureText = "+DepartureText+", DepartureTime = "+DepartureTime+", RouteDirection = "+RouteDirection+", Description = "+Description+", VehicleLatitude = "+VehicleLatitude+", Actual = "+Actual+", Gate = "+Gate+", BlockNumber = "+BlockNumber+", Terminal = "+Terminal+", Route = "+Route+", VehicleHeading = "+VehicleHeading+"]";
-    }
+    public String departure_text;
+    public String description;
+    public String route_id;
 }
